efe7ae9e37d705f4aa18706718b40ea179edc56f
Dan Ott <dan@dtott.com>
2023-04-27 08:56:20 -0400

    add pages and layout

--- app/csp/page.tsx
+++ app/csp/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          CSP Report Only Example
+          Full CSP{' '}
+          <small>
+            (<code>content-security-policy</code>)
+          </small>
         </h2>
       </div>
 
