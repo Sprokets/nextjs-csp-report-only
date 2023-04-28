48d34f2fa1c2a8d667c652e5e6bcd57f3b4581c4
Dan Ott <dan@dtott.com>
2023-04-28 12:48:05 -0400

    Add CSP header

---
+++ middleware.ts
@@ -0,0 +1,74 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+export function generateCsp() {
+  // generate random nonce converted to base64. Must be different on every HTTP page load
+  // const nonce = crypto.randomBytes(16).toString('base64')
+  const nonce = crypto.randomUUID();
+
+  const csp = [
+    { name: 'default-src', values: ["'self'"] },
+    {
+      name: 'script-src',
+      values: [
+        "'report-sample'",
+        "'self'",
+        `'nonce-${nonce}'`,
+        "'strict-dynamic'",
+      ],
+    },
+    {
+      name: 'style-src',
+      values: ["'report-sample'", "'self'", `'nonce-${nonce}'`],
+    },
+    {
+      name: 'connect-src',
+      values: ["'self'", '*.vercel-insights.com'],
+    },
+    { name: 'font-src', values: ["'self'", 'data:'] },
+    { name: 'img-src', values: ["'self'", 'data:'] },
+    { name: 'worker-src', values: ["'self'", 'blob:'] },
+    { name: 'frame-ancestors', values: ["'none'"] },
+    { name: 'form-action', values: ["'self'"] },
+  ];
+
+  const cspString = csp
+    .map((directive) => {
+      return `${directive.name} ${directive.values.join(' ')}`;
+    })
+    .join('; ');
+
+  return { csp: cspString, nonce };
+}
+
+export async function middleware(request: NextRequest) {
+  // generate CSP and nonce
+  const { csp, nonce } = generateCsp();
+
+  // Clone the request headers
+  const requestHeaders = new Headers(request.headers);
+
+  // set nonce request header to read in pages if needed
+  requestHeaders.set('x-nonce', nonce);
+
+  // set CSP header
+  const headerKey =
+    request.nextUrl.pathname === '/csp-report-only'
+      ? 'content-security-policy-report-only'
+      : 'content-security-policy';
+
+  // Set the CSP header so that `app-render` can read it and generate tags with the nonce
+  requestHeaders.set(headerKey, csp);
+
+  // create new response
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  });
+
+  // Also set the CSP so that it is outputted to the browser
+  response.headers.set(headerKey, csp);
+
+  return response;
+}
